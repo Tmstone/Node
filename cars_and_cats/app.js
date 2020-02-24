a96b22ff6e0199690d887f76f1178d2b33668a63
Terence Stone <terence.stone25@gmail.com>
2020-02-24 16:46:33 -0500

    initial commit for cats and cars

---
+++ cars_and_cats/app.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function(request, response) {
+    if(request.url === '/') {
+        fs.readFile('./views/index.html', 'utf8', function (errors, contents) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    //css routing here
+    else if (request.url === '/stylesheets/style.css') {
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'text/css'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === "/cars") {
+         fs.readFile('./views/cars.html', 'utf8', (errors, contents) => {
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    //serve images
+    else if (request.url === '/images/image here') {
+        fs.readFile('/images/image here', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    //serve cats
+    else if (request.url === "/cats") {
+        fs.readFile('./views/cats.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+   }
+    else {
+        response.end('File not found!!!');
+    }
+});
+server.listen(7077);
+console.log("listening on port 7077");
