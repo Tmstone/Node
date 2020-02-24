ce1d826ead91b937ee3b7eb4b7e88b3acdc3923e
Terence Stone <terence.stone25@gmail.com>
2020-02-24 15:28:30 -0500

    adding css routing to server

--- node_server/app.js
+++ node_server/app.js
@@ -12,6 +12,14 @@ const server = http.createServer(function (request, response){
             response.end(); // finished!
         });
     }
+    else if(request.url === '/static/stylesheets/style.css') {
+        fs.readFile('index.html', function (errors, contents){
+            //fs.readFile('/static/stylesheet/style.css', static('static'))
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
     // request didn't match anything:
     else {
         response.writeHead(404);
