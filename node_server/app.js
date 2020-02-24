768638114878b64cb1841062c47ac778cf463c24
Terence Stone <terence.stone25@gmail.com>
2020-02-24 00:26:56 -0500

    updated app.js

--- node_server/app.js
+++ node_server/app.js
@@ -6,7 +6,7 @@ const server = http.createServer(function (request, response){
     // this is how we do routing:
     if(request.url === '/') {
         fs.readFile('index.html', 'utf8', function (errors, contents){
-            app.use('/static', express.static('static'))
+            //fs.readFile('/static/stylesheet/style.css', static('static'))
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
