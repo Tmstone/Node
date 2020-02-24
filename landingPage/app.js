a1a1686a06cf2c89938e2ac65cf016df3d32898f
Terence Stone <terence.stone25@gmail.com>
2020-02-24 12:48:37 -0500

    adding routes to the node server for dojos and ninjas

--- landingPage/app.js
+++ landingPage/app.js
@@ -18,6 +18,22 @@ const server = http.createServer(function (request, response){
             response.end(); // finished!
         });
     }
+    else if(request.url === '/ninjas') {
+        fs.readFile('ninja.html', 'utf8', function (errors, contents){
+            //fs.readFile('/static/stylesheet/style.css', static('static'))
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/dojos/new') {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            //fs.readFile('/static/stylesheet/style.css', static('static'))
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
     // request didn't match anything:
     else {
         response.writeHead(404);
