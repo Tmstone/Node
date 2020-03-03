3d27665c975e814eb80c2929d63adc83c77f4c4d
Terence Stone <terence.stone25@gmail.com>
2020-03-03 12:35:06 -0500

    updated the mathlib files

--- math_module/app.js
+++ math_module/app.js
@@ -1,1 +1,5 @@
 const mathlib = require('./mathlib')();
+mathlib.add(45, 10);
+mathlib.multiply(3,13);
+mathlib.square(25);
+mathlib.random(2,5)
--- math_module/mathlib.js
+++ math_module/mathlib.js
@@ -1,16 +1,16 @@
 module.exports = function() {
     return {
         add: function(num1, num2) {
-
+            console.log('The sum of ' + num1 + ' & ' + num2 + ' is: '+ (num1 + num2));
         },
         multiply: function (num1, num2) {
-
+            console.log('The product of ' + num1 + ' & ' + num2 + ' is: '+ num1 * num2);
         },
         square: function (num) {
-
+            console.log('The square of ' + num + ' is: '+ num * num);
         },
         random: function (num1, num2) {
-
+            console.log(Math.floor(Math.random() * num2)+ num1)
         }
     }
 };
