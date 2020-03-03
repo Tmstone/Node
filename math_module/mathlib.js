c135ad427658816bde0238dc1d4997bd3189c1ef
Terence Stone <terence.stone25@gmail.com>
2020-03-02 23:12:04 -0500

    initial commit for mathlib.js

---
+++ math_module/mathlib.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+    return {
+        add: function(num1, num2) {
+
+        },
+        multiply: function (num1, num2) {
+
+        },
+        square: function (num) {
+
+        },
+        random: function (num1, num2) {
+
+        }
+    }
+};
